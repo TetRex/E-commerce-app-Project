59491f4def52a4b03068563c66abb0caf6d2c789
OTOfngn <c98423795274152963@outlook.com>
2025-10-27 10:34:21 +0200

    profile

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+dynamic bgcolor = Colors.white;
+dynamic fgcolor = Colors.deepOrange; 
+dynamic buttoncolor = Color.fromARGB(255, 100, 100, 100);
+void main() {
+  runApp(Profile());
+}
+
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
@@ -8,14 +15,17 @@ class Profile extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: bgcolor,
+        appBar: AppBar(
+          title: Text('My Profile'),
 
-
+        ),
         body:  Row(
           children: [
             Text('Hello World')
           ],
         ),
-          ),
+      ),
     );
   }
 } 
